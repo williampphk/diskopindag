95fdd1fbaa546c6325967b40eb7749d880152e15
williampphk <william.pphk@gmail.com>
2022-12-01 16:59:55 +0700

    menambahkan section daftar event (list dan card

--- js/javas.js
+++ js/javas.js
@@ -5,3 +5,7 @@ tanggal = date.getDate();
 bulan = date.getMonth();
 tahun = date.getFullYear();
 document.write(tanggal + "-" + arrbulan[bulan] + "-" + tahun);
+
+const target = document.querySelector(".target");
+const links = document.querySelectorAll(".mynav a");
+const colors = ["deepskyblue", "orange", "firebrick", "gold", "magenta", "black", "darkblue"];
