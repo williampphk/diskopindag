e624fb0e80bf6e0e97c7ca1c84a6ebe6ac160204
williampphk <william.pphk@gmail.com>
2022-12-05 19:31:10 +0700

    membuat navbar dan topbar menggunakan framework laravel

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://diskopindag.malangkota.go.id/css/styles.min.css" />
     <link rel="stylesheet" href="https://diskopindag.malangkota.go.id/css/boxicons.min.css" />
     <!-- css -->
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="style.css" />
     <!-- css bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
@@ -23,72 +23,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" />
   </head>
   <body id="body" data-bs-spy="scroll" data-bs-target="#mainNav" data-bs-offset="0" tabindex="0">
-    <!-- Topbar -->
-    <nav class="topbar">
-      <div class="container">
-        <ul class="topbar__nav">
-          <li class="topbar__item">
-            <a href="https://malangkota.go.id/" target="_blank">Pemkot Malang</a>
-          </li>
-          <li class="topbar__item">
-            <a href="http://kota.directory/malang-directory/" target="_blank">Direktori</a>
-          </li>
-          <li class="topbar__item">
-            <a href="https://smartcity.malangkota.go.id/" target="_blank">Smart City</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <!-- topbar end -->
-
+    
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top mt-5" id="mainNav">
-      <div class="container">
-        <a class="navbar-brand" href="/">
-          <img src="https://diskopindag.malangkota.go.id/image/site/1622779402_logo.png" alt="DISKOPINDAG MALANG" />
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <i class="bx bx-menu-alt-right"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="/">Beranda</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/profile">Profil</a>
-            </li>
-            <li class="nav-item dropdown" data-toggle="hover">
-              <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Layanan</a>
-              <div class="dropdown-menu">
-                <a href="https://sambat.malangkota.go.id/" class="dropdown-item">Kritik dan Saran</a>
-                <a href="https://sembako.malangkota.go.id/" class="dropdown-item">Sembako</a>
-                <a href="https://metrologi.malangkota.go.id/" class="dropdown-item">Metrologi</a>
-                <a href="https://pasarmbois.malangkota.go.id/" class="dropdown-item">Marketplace UMKM</a>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/information">Informasi</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" href="/event">Event</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/news">Berita</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#kontak">Kontak</a>
-            </li>
-            <li class="nav-item">
-              <a id="btn" class="nav-link" href="#kontak"><button type="button" class="btn btn-outline-primary">Daftar</button></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#kontak"><button type="button" class="btn btn-primary">Login</button></a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+   @include('partials.navbar')
     <!-- navbar end -->
 
     <!-- Search overlay -->
